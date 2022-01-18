9a82cc013fd9dc0795281031389bf0a704cd7b91
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-18 18:03:14 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.scss';
+import React, { useState } from "react";
+import axios from "axios";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import Popup from "./components/Popup";
+import "./App.scss";
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+  };
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+  };
+
+  const openPopup = (id) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState((prevState) => {
+        return { ...prevState, selected: result };
+      });
+    });
+  };
+
+  const closePopup = () => {
+    setState((prevState) => {
+      return { ...prevState, selected: {} };
+    });
+  };
+
   return (
-    <div className="App c-app">
+    <div className="App">
       <header>
-        <h1>Header</h1>
+        <h1>Ask To Movie</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+        <Results results={state.results} openPopup={openPopup} />
+        {typeof state.selected.Title != "undefined" ? (
+          <Popup selected={state.selected} closePopup={closePopup} />
+        ) : (
+          false
+        )}
+      </main>
     </div>
   );
 }
